08b737a550a5151ef9590be3f12922700b0e4277
Jose Ramirez <joseramirez@Jose-Ramirez-MacBook-Pro.local>
2020-08-21 15:13:09 -0500

    Implement index with posts table

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: "test@test.com", password: "testing1234",
+    @user = User.create(email: "test@test.com", password: "testing1234",
       password_confirmation: "testing1234",first_name: "jose", last_name:"Ramirez")
-    login_as(user, :scope => :user)
+    login_as(@user, :scope => :user)
     visit new_post_path
   end
 
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has posts title' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create(email: "test@test.com", password: "testing1234", password_confirmation: "testing1234",
+        first_name: "jose", last_name:"Ramirez")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
     end
 
     it "can be created" do

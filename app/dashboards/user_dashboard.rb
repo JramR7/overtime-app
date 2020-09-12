c20b82b15dcfa827e3c0037c335b80a7213495b8
Jose Ramirez <joseramirez@Jose-Ramirez-MacBook-Pro.local>
2020-09-12 12:44:13 -0500

    Improve search on dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -3,18 +3,18 @@ require "administrate/base_dashboard"
 class UserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     posts: Field::HasMany,
-    id: Field::Number,
-    email: Field::String,
-    password: Field::String,
-    sign_in_count: Field::Number,
-    current_sign_in_at: Field::DateTime,
-    last_sign_in_at: Field::DateTime,
+    id: Field::Number.with_options(searchable: false),
+    email: Field::String.with_options(searchable: true),
+    password: Field::String.with_options(searchable: false),
+    sign_in_count: Field::Number.with_options(searchable: false),
+    current_sign_in_at: Field::DateTime.with_options(searchable: false),
+    last_sign_in_at: Field::DateTime.with_options(searchable: false),
     current_sign_in_ip: Field::String.with_options(searchable: false),
     last_sign_in_ip: Field::String.with_options(searchable: false),
-    first_name: Field::String,
-    last_name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    first_name: Field::String.with_options(searchable: true),
+    last_name: Field::String.with_options(searchable: true),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
     type: Field::String,
   }.freeze
 
@@ -22,6 +22,8 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
+    :first_name,
+    :last_name,
     :type
   ].freeze
 
